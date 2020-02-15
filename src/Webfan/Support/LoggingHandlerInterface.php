75a4354ca82d51852fafba0cc6cb2e7b38fa7591
Till Wehowski <github@webfan.de>
2020-02-15 02:18:24 +0100

    Create LoggingHandlerInterface.php

---
+++ src/Webfan/Support/LoggingHandlerInterface.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Webfan\Support;
+
+interface LoggingHandlerInterface
+{
+
+  public function pune();
+  public function register();
+  public function prepend_error_handler(callable $fn);
+  public function set_error_handler(callable $fn);
+  public function get_error_handler();
+  public function prepend_exception_handler(callable $fn);
+  public function set_exception_handler(callable $fn);
+  public function get_exception_handler();
+  public function restore_php_exception_handler();
+  public function restore_php_error_handler();
+
+}
+
