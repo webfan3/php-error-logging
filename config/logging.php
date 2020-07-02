e3fedd5674a21ad94898de667eb8d82a2d9f8eb5
Till Wehowski <github@webfan.de>
2020-07-02 07:08:45 +0200

    Create logging.php

---
+++ config/logging.php
@@ -0,0 +1,43 @@
+<?php
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+use Franzl\Middleware\Whoops\WhoopsMiddleware;
+
+return [
+   Run::class  => function (ContainerInterface $c) {
+             $run = new Run();
+             
+             return $run;
+          },	
+  'logger.project-shield.php' => function (ContainerInterface $c) {
+             return $c->get(Run::class);          
+     },	
+                   
+    PrettyPageHandler::class  => function (ContainerInterface $c) {
+             $Pretty = new PrettyPageHandler();
+             
+         return $Pretty;
+     },	  
+          
+	'state.emitter' => decorate(function($emitter, ContainerInterface $c){
+	   
+     $emitter->once('bootstrap', static function($eventName, $emitter, $Event) use($c){
+			  $logger = $Event->getArgument('container')->get('logger.project-shield.php');
+        
+        $logger >register(); 
+        
+		 });
+     
+		return $emitter;
+	}),
+	
+  
+  
+	\frdl\web\RequestHandler::class => decorate(function($handler, ContainerInterface $c){
+		$handler = $handler->addMiddleware(new WhoopsMiddleware());
+		
+		return $handler;
+	}),
+
+];
