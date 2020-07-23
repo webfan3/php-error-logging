6a597c5c3f03ffe38620fa8bb0113421a2f5df9a
Till Wehowski <github@webfan.de>
2020-07-24 01:03:54 +0200

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -10,6 +10,38 @@ use Whoops\Run;
 use Franzl\Middleware\Whoops\WhoopsMiddleware;
 
 return [
+	
+	
+	    'logger.php.exception_handler' => function (ContainerInterface $c) {
+		  return [$c->get(\Psr\Http\Server\RequestHandlerInterface::class),'withErrorResponse'];
+		}, 
+	    'logger.php' => get(\Webfan\Support\PhpLogs::class), 
+	    \Webfan\Support\PhpLogs::class  => function (ContainerInterface $c) {
+			
+            $f = __DIR__ .\DIRECTORY_SEPARATOR.'..'.\DIRECTORY_SEPARATOR.'..'.\DIRECTORY_SEPARATOR.'..'.\DIRECTORY_SEPARATOR
+			   .'config'. \DIRECTORY_SEPARATOR.'module-configs'.\DIRECTORY_SEPARATOR.'frdl.web'.\DIRECTORY_SEPARATOR
+			   .'preferences.json';
+           $preferences = (file_exists($f))
+	           ? json_decode(file_get_contents($f))
+	           : json_decode(file_get_contents(__DIR__ .\DIRECTORY_SEPARATOR.'..'.\DIRECTORY_SEPARATOR.'..'.\DIRECTORY_SEPARATOR.'..'.\DIRECTORY_SEPARATOR.$c->get('project')->modules_dirname.\DIRECTORY_SEPARATOR.'frdl.web'.\DIRECTORY_SEPARATOR.'preferences.json'));
+			
+			
+			return new \Webfan\Support\PhpLogs([
+		         'logs.debug' => $preferences->logs->debug, 
+		         'logs.dir' => rtrim($c->get('project')->dir, \DIRECTORY_SEPARATOR.'/ ')
+		                       .\DIRECTORY_SEPARATOR.'logs'.\DIRECTORY_SEPARATOR.'frdl'.\DIRECTORY_SEPARATOR,
+		         'logs.prune_size.trigger' => 999999,
+		         'logs.prune_size.size' =>    32000,
+	             'logs.autoprune' =>    true,
+		         'logs.file.errors' => 'error.log',
+		         'logs.file.memory' => 'memory.log',
+		         'logs.error_level' => intval($preferences->logs->error_level),
+		         'logs.log_level' => intval($preferences->logs->log_level),
+		         'logs.error_reporting' => \E_ALL,
+		         'logs.display_errors' => intval($preferences->logs->display_errors),
+            ], true, $c->get('logger.php.exception_handler'));
+          },	
+	
    Run::class  => function (ContainerInterface $c) {
              $run = new Run();
              
